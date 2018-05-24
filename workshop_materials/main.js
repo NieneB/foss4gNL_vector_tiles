b526d4c9486ad82e44dd10e5675355ec39f9d596
NieneB <niene.boeijen@gmail.com>
2018-05-24 14:49:50 +0200

    workshop_materials

---
+++ workshop_materials/main.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmllbmViIiwiYSI6IlR6NWEzdmMifQ.dRVGdAluvTb9EIXVBREbzw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/nieneb/cjg3h8yp80oi82rldxukpu0oi',
+
+});
